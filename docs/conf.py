2ec7ef11be7c580873d086c0b0479d71b9f1c011
Dmitry Maztartsev <DmitryBowie@gmail.com>
2015-02-07 16:29:59 +0300

    Опубликовано

--- docs/conf.py
+++ docs/conf.py
@@ -185,7 +185,7 @@ htmlhelp_basename = 'MathShortdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 'pointsize': '12pt',
